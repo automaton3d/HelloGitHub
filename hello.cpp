d328558847fd569e6d8c34320ad67173333f276f
javaresende <javaresende@gmail.com>
2025-11-05 20:54:28 -0300

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 5 de nov. de 2025
+ *      Author: Alexandre
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, GitHub!" << endl;
+	return 0;
+}
